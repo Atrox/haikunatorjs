6c0f7105a5aebe04266c5b33e92a995dc950ac33
Atrox <mail@atrox.me>
2016-09-24 20:54:14 +0200

    Class based (v2) (#10)
    * preparation for v2.0.0, class based
    
    * 2.0.0-beta.1 - custom adjectives and nouns added, tests improved
    
    * remove duplicated noun

--- src/haikunator.js
+++ src/haikunator.js
@@ -1,8 +1,13 @@
+'use strict'
+
+import RandomGenerator from 'random-seed'
+import setDefaults from 'lodash.defaults'
+
 /**
  * Adjectives used by haikunator
  * @type {string[]}
  */
-const adjectives = [
+const defaultAdjectives = [
   'aged', 'ancient', 'autumn', 'billowing', 'bitter', 'black', 'blue', 'bold',
   'broad', 'broken', 'calm', 'cold', 'cool', 'crimson', 'curly', 'damp',
   'dark', 'dawn', 'delicate', 'divine', 'dry', 'empty', 'falling', 'fancy',
@@ -15,13 +20,13 @@ const adjectives = [
   'square', 'steep', 'still', 'summer', 'super', 'sweet', 'throbbing', 'tight',
   'tiny', 'twilight', 'wandering', 'weathered', 'white', 'wild', 'winter', 'wispy',
   'withered', 'yellow', 'young'
-];
+]
 
 /**
  * Nouns used by haikunator
  * @type {string[]}
  */
-const nouns = [
+const defaultNouns = [
   'art', 'band', 'bar', 'base', 'bird', 'block', 'boat', 'bonus',
   'bread', 'breeze', 'brook', 'bush', 'butterfly', 'cake', 'cell', 'cherry',
   'cloud', 'credit', 'darkness', 'dawn', 'dew', 'disk', 'dream', 'dust',
@@ -33,47 +38,74 @@ const nouns = [
   'river', 'salad', 'scene', 'sea', 'shadow', 'shape', 'silence', 'sky',
   'smoke', 'snow', 'snowflake', 'sound', 'star', 'sun', 'sun', 'sunset',
   'surf', 'term', 'thunder', 'tooth', 'tree', 'truth', 'union', 'unit',
-  'violet', 'voice', 'water', 'water', 'waterfall', 'wave', 'wildflower', 'wind',
-  'wood'
-];
+  'violet', 'voice', 'water', 'waterfall', 'wave', 'wildflower', 'wind', 'wood'
+]
 
 /**
- * Generate heroku-like random names
- *
- * @param {string} [delimiter=-]
- * @param {number} [tokenLength=4]
- * @param {boolean} [tokenHex=false]
- * @param {string} [tokenChars=0123456789]
- * @param {string} [seed]
- * @returns {string}
+ * Default options used by haikunate method
+ * @type {{delimiter: string, tokenLength: number, tokenHex: boolean, tokenChars: string}}
  */
-export default function haikunate({delimiter = '-', tokenLength = 4, tokenHex = false, tokenChars = '0123456789', seed} = {}) {
-  if (tokenHex) {
-    tokenChars = '0123456789abcdef';
+const defaultOptions = {
+  delimiter: '-',
+  tokenLength: 4,
+  tokenHex: false,
+  tokenChars: '0123456789'
+}
+
+export default class Haikunator {
+  /**
+   * Initialize new haikunator
+   * @param {object} defaults
+   * @param {string[]} adjectives
+   * @param {string[]} nouns
+   * @param {string} seed
+   */
+  constructor ({ defaults = {}, adjectives = defaultAdjectives, nouns = defaultNouns, seed } = {}) {
+    this.adjectives = adjectives
+    this.nouns = nouns
+
+    this.random = new RandomGenerator(seed)
+    this.config = setDefaults(defaults, defaultOptions)
   }
 
-  // determine the random function to use
-  let random;
-  if (seed) {
-    let RandomGenerator = require('random-seed');
-    random = new RandomGenerator(seed).random;
-  } else {
-    random = Math.random;
+  /**
+   * Generate heroku-like random names
+   * @param {object} options
+   * @returns {string}
+   */
+  haikunate (options = {}) {
+    // set specified options
+    const config = setDefaults(options, this.config)
+
+    // set tokenChars if tokenHex is set
+    if (config.tokenHex === true) {
+      config.tokenChars = '0123456789abcdef'
+    }
+
+    // pick adjective and noun
+    const adjective = this._randomElement(this.adjectives)
+    const noun = this._randomElement(this.nouns)
+
+    // create random token
+    let token = ''
+    for (let i = 0; i < config.tokenLength; i++) {
+      token += this._randomElement(config.tokenChars)
+    }
+
+    // create result and return
+    const sections = [ adjective, noun, token ]
+    return sections.filter(e => {
+      return e === 0 || e
+    }).join(config.delimiter)
   }
 
-  // pick adjective and noun
-  const adjective = adjectives[Math.floor(random() * adjectives.length)];
-  const noun = nouns[Math.floor(random() * nouns.length)];
-
-  // create hex token
-  let token = '';
-  for (let i = 0; i < tokenLength; i++) {
-    token += tokenChars.charAt(Math.floor(random() * tokenChars.length));
+  /**
+   * Get a random element from an array/string
+   * @param {(string|Array)} array
+   * @returns {*}
+   * @private
+   */
+  _randomElement (array) {
+    return array[ this.random(array.length) ]
   }
-
-  // create result and return
-  const sections = [adjective, noun, token];
-  return sections.filter((e) => {
-    return e === 0 || e;
-  }).join(delimiter);
 }
--- test/test.js
+++ test/test.js
@@ -1,86 +1,80 @@
-import test from "ava";
-import haikunate from "../src/haikunator";
+'use strict'
 
-test('should return 4 digits', t => {
-  t.regex(haikunate(), /((?:[a-z][a-z]+))(-)((?:[a-z][a-z]+))(-)(\d{4})$/i);
-});
+import test from 'ava'
+import Haikunator from '../src/haikunator'
 
-test('should return 4 digits as hex', t => {
-  const opts = {
-    tokenHex: true
-  };
+const haikunator = new Haikunator()
 
-  t.regex(haikunate(opts), /((?:[a-z][a-z]+))(-)((?:[a-z][a-z]+))(-)(.{4})$/i);
-});
+test('general functionality', t => {
+  const tests = [
+    [ {}, /[a-z]+-[a-z]+-[0-9]{4}$/ ],
+    [ { tokenHex: true }, /[a-z]+-[a-z]+-[0-f]{4}$/ ],
+    [ { tokenLength: 9 }, /[a-z]+-[a-z]+-[0-9]{9}$/ ],
+    [ { tokenLength: 9, tokenHex: true }, /[a-z]+-[a-z]+-[0-f]{9}$/ ],
+    [ { tokenLength: 0 }, /[a-z]+-[a-z]+$/ ],
+    [ { delimiter: '.' }, /[a-z]+.[a-z]+.[0-9]{4}$/ ],
+    [ { tokenLength: 0, delimiter: ' ' }, /[a-z]+ [a-z]+/ ],
+    [ { tokenLength: 0, delimiter: '' }, /[a-z]+$/ ],
+    [ { tokenChars: 'xyz' }, /[a-z]+-[a-z]+-[x-z]{4}$/ ]
+  ]
 
-test('should return 9 digits', t => {
-  const opts = {
-    tokenLength: 9
-  };
-
-  t.regex(haikunate(opts), /((?:[a-z][a-z]+))(-)((?:[a-z][a-z]+))(-)(\d{9})$/i);
-});
-
-test('should return 9 digits as hex', t => {
-  const opts = {
-    tokenLength: 9,
-    tokenHex: true
-  };
-
-  t.regex(haikunate(opts), /((?:[a-z][a-z]+))(-)((?:[a-z][a-z]+))(-)(.{9})$/i);
-});
-
-test('drops token if token range is 0', t => {
-  const opts = {
-    tokenLength: 0
-  };
-
-  t.regex(haikunate(opts), /((?:[a-z][a-z]+))(-)((?:[a-z][a-z]+))$/i);
-});
-
-test('permits optional configuration of the delimiter', t => {
-  const opts = {
-    delimiter: '.'
-  };
-
-  t.regex(haikunate(opts), /((?:[a-z][a-z]+))(\.)((?:[a-z][a-z]+))(\.)(\d+)$/i);
-});
-
-test('drops the token if token range is 0 and delimiter is an empty space', t => {
-  const opts = {
-    tokenLength: 0,
-    delimiter: ' '
-  };
-
-  t.regex(haikunate(opts), /((?:[a-z][a-z]+))( )((?:[a-z][a-z]+))$/i);
-});
-
-test('returns one single word if token and delimiter are dropped', t => {
-  const opts = {
-    tokenLength: 0,
-    delimiter: ''
-  };
-
-  t.regex(haikunate(opts), /((?:[a-z][a-z]+))$/i);
-});
-
-test('permits custom token chars', t => {
-  const opts = {
-    tokenChars: 'A'
-  };
-
-  t.regex(haikunate(opts), /((?:[a-z][a-z]+))(-)((?:[a-z][a-z]+))(-)(AAAA)$/i);
-});
+  tests.forEach(test => {
+    t.regex(haikunator.haikunate(test[ 0 ]), test[ 1 ])
+  })
+})
 
 test('wont return the same name for subsequent calls', t => {
-  t.not(haikunate(), haikunate());
-});
+  const tests = [ new Haikunator(), new Haikunator() ]
+
+  tests.forEach(h1 => {
+    tests.forEach(h2 => {
+      t.not(h1.haikunate(), h2.haikunate())
+    })
+  })
+})
 
 test('returns the same name if seed is provided', t => {
-  const opts = {
-    seed: 'foo'
-  };
+  const seed = 'definitively random seed'
 
-  const [haiku1, haiku2] = [haikunate(opts), haikunate(opts)];
-  t.is(haiku1, haiku2);
-});
+  const h1 = new Haikunator({ seed: seed })
+  const h2 = new Haikunator({ seed: seed })
+
+  t.is(h1.haikunate(), h2.haikunate())
+  t.is(h1.haikunate(), h2.haikunate())
+})
+
+test('class wide defaults', t => {
+  const wordHaikunator = new Haikunator({
+    defaults: {
+      tokenLength: '0',
+      delimiter: ''
+    }
+  })
+
+  t.regex(wordHaikunator.haikunate(), /((?:[a-z][a-z]+))$/i)
+})
+
+test('class wide defaults can get overridden by function parameters', t => {
+  const wordHaikunator = new Haikunator({
+    defaults: {
+      tokenLength: 0,
+      delimiter: ''
+    }
+  })
+
+  const opts = {
+    delimiter: '.'
+  }
+
+  t.regex(wordHaikunator.haikunate(), /[a-z]+$/)
+  t.regex(wordHaikunator.haikunate(opts), /[a-z]+.[a-z]+$/)
+})
+
+test('custom adjectives & nouns', t => {
+  const customHaikunator = new Haikunator({
+    adjectives: [ 'adjective' ],
+    nouns: [ 'noun' ]
+  })
+
+  t.regex(customHaikunator.haikunate(), /adjective-noun-\d{4}$/)
+})
